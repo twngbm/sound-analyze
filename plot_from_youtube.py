471361815a25b53844bf991add2d7a0acb485d65
twngbm <twngbm@gmail.com>
2017-10-29 23:56:41 +0800

    add exe,add default URL,fix some bug

--- plot_from_youtube.py
+++ plot_from_youtube.py
@@ -2,93 +2,19 @@ from __future__ import unicode_literals
 import youtube_dl
 import numpy as np
 import wave
-import time
 import math
 import struct
 from math import sqrt
 import pyaudio
 import terminalsize
-import time
+
+fft_averages = []
 
 
-def ydl(URL):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl':'ydlaudiodata.%(ext)s',
-        'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'wav',
-        'preferredquality': '320',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([URL])
+def getBandWidth():
+    return (2.0 / sample_size) * (sample_rate / 2.0)
 
-print("input an URL from youtube(https://):")
-s=input()
-if s!="":
-    ydl(s)
-
-
-
-
-terminalsize=terminalsize.get_terminal_size()
-print("Download Finished.")
-print("Processing...please wait.")
-print("This may take a few minutes")
-if __name__ == '__main__':
-    # Open the wave file and get info
-    wave_file = wave.open('ydlaudiodata.wav', 'r')
-    data_size = wave_file.getnframes()
-    channel=wave_file.getnchannels()
-    sample_rate = wave_file.getframerate()
-    sample_width = wave_file.getsampwidth()
-    #print(wave_file.getparams())
-    sample_width = wave_file.getsampwidth()
-    duration = data_size / float(sample_rate)
-
-    # Read in sample data
-    print(wave_file.getparams())
-
-    # Close the file, as we don't need it any more
-    wave_file.close()
-
-    # Unpack the binary data into an array
-    
-   
-    
-
-    # Process many samples
-    fouriers_per_second = 24  # Frames per second
-    fourier_spread = 1.0 / fouriers_per_second
-    fourier_width = fourier_spread
-    fourier_width_index = fourier_width * float(sample_rate)
-    length_to_process = int(duration) - 1
-    
-    
-    #print ("Fourier width: %s" % str(fourier_width))
-    
-    total_transforms = int(round(length_to_process * fouriers_per_second))
-    fourier_spacing = round(fourier_spread * float(sample_rate))
-    
-    print ("Duration: %s" % duration)
-    print ("For Fourier width of " + str(fourier_width) +
-           " need " + str(fourier_width_index) + " samples each FFT")
-    print ("Doing " + str(fouriers_per_second) + " Fouriers per second")
-    print ("Total " + str(total_transforms * fourier_spread))
-    print ("Spacing: " + str(fourier_spacing))
-    print ("Total transforms " + str(total_transforms))
-    
-    lastpoint = int(
-        round(length_to_process * float(sample_rate) + fourier_width_index)) - 1
-
-    sample_size = fourier_width_index
-    freq = sample_rate / sample_size * np.arange(sample_size)
-
-    def getBandWidth():
-        return (2.0 / sample_size) * (sample_rate / 2.0)
-
-    def freqToIndex(f):
+def freqToIndex(f):
         # If f (frequency is lower than the bandwidth of spectrum[0]
         if f < getBandWidth() / 2:
             return 0
@@ -96,11 +22,9 @@ if __name__ == '__main__':
             return sample_size - 1
         fraction = float(f) / float(sample_rate)
         index = round(sample_size * fraction)
-        return index
+        return index        
 
-    fft_averages = []
-
-    def average_fft_bands(fft_array):
+def average_fft_bands(fft_array):
         num_bands = 12  # The number of frequency bands (12 = 1 octave)
         del fft_averages[:]
         for band in range(0, num_bands):
@@ -120,11 +44,86 @@ if __name__ == '__main__':
 
             avg /= (hiBound - lowBound + 1)
             fft_averages.append(avg)
-    input("Prass Enter To start,wear your headphone!!")
 
-    space=int(terminalsize[0]/12)
-    empty_string=" "*space
-   
+def ydl(URL):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl':'ydlaudiodata.%(ext)s',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '320',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([URL])    
+
+
+
+while True:
+    while True:
+        print("input an URL or vedio code from youtube(https:// or v):")
+        print("Or input 1 to listen to Faded.")
+        s=input()
+        if s!='':
+            if s[0]=='h':
+                ydl(s)
+            elif s[0]=='v':
+                s='https://www.youtube.com/watch?'+s
+                print(s)
+                ydl(s)
+                
+            elif s[0]=="1":
+                ydl('https://www.youtube.com/watch?v=60ItHLz5WEA')
+        try:
+            wave_file = wave.open('ydlaudiodata.wav', 'r')
+            break
+        except:
+            print("You don't enter the url and local has no file.")
+            print("please reenter again.")
+
+    print("Download Finished.")
+    print("Processing...please wait.")
+    # Open the wave file and get info
+    
+    data_size = wave_file.getnframes()
+    channel=wave_file.getnchannels()
+    sample_rate = wave_file.getframerate()
+    sample_width = wave_file.getsampwidth()
+    sample_width = wave_file.getsampwidth()
+    duration = data_size / float(sample_rate)
+    print(wave_file.getparams())
+
+    # Close the file, as we don't need it any more
+    wave_file.close()
+
+    # Process many samples
+    fouriers_per_second = 24  # Frames per second
+    fourier_spread = 1.0 / fouriers_per_second
+    fourier_width = fourier_spread
+    fourier_width_index = fourier_width * float(sample_rate)
+    length_to_process = int(duration) - 1
+
+    
+    total_transforms = int(round(length_to_process * fouriers_per_second))
+    fourier_spacing = round(fourier_spread * float(sample_rate))
+    
+    print ("Duration: %s" % duration)
+    print ("For Fourier width of " + str(fourier_width) +
+           " need " + str(fourier_width_index) + " samples each FFT")
+    print ("Doing " + str(fouriers_per_second) + " Fouriers per second")
+    print ("Total " + str(total_transforms * fourier_spread))
+    print ("Spacing: " + str(fourier_spacing))
+    print ("Total transforms " + str(total_transforms))
+    
+    lastpoint = int(
+        round(length_to_process * float(sample_rate) + fourier_width_index)) - 1
+
+    sample_size = fourier_width_index
+    freq = sample_rate / sample_size * np.arange(sample_size)
+
+    input("Prass Enter To start,wear your headphone!!")
     wave_file=wave.open('ydlaudiodata.wav','r')
     p=pyaudio.PyAudio()
     chunk=2048
@@ -136,6 +135,9 @@ if __name__ == '__main__':
     data = wave_file.readframes(chunk)
     print(wave_file.getparams())
     while data!='':
+        terminalsize_t=terminalsize.get_terminal_size()
+        space=int(terminalsize_t[0]/12)
+        empty_string=" "*space
         stream.write(data)
         data = wave_file.readframes(chunk)
         #print(data,"\n")
@@ -149,7 +151,7 @@ if __name__ == '__main__':
         try:
             ori_sound_data = struct.unpack(unpack_fmt, data)
         except:
-            quit()
+            break
         sound_data=[]
         #print(len(ori_sound_data))
         
@@ -165,15 +167,15 @@ if __name__ == '__main__':
         #print(fft_averages)####
         fft_normalize=[]        
         for i in fft_averages:
-            max_height=int(i/terminalsize[1])
-            if max_height>terminalsize[1]:
-                max_height=terminalsize[1]
+            max_height=int(i/terminalsize_t[1])
+            if max_height>terminalsize_t[1]:
+                max_height=terminalsize_t[1]
             fft_normalize.append(max_height)
         #print("  ",fft_normalize,"  ")
         word_line=""
         
-        for i in range(terminalsize[1]+1):
-            height=terminalsize[1]-i
+        for i in range(terminalsize_t[1]+1):
+            height=terminalsize_t[1]-i
             for j in range(len(fft_normalize)-1):
                 if fft_normalize[j]<height:
                     word_line+=" "
@@ -187,3 +189,5 @@ if __name__ == '__main__':
                         word_line+="\n"
         print(word_line)
                         
+
+
